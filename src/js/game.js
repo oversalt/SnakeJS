1c1d419b71aaa566d76d22cdeb77684890e6f47c
Daven Paul Casia <casia3115@saskpolytech.ca>
2017-03-28 18:29:33 -0600

    created the canvas for the game

--- src/js/game.js
+++ src/js/game.js
@@ -1,3 +1,5 @@
+//This file will be the entry point of all JavaScript files
+// found in this game 
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload() {
