302a42a0b9e07a7a253bfe2a4ba282b97b65899e
Daven Paul Casia <casia3115@saskpolytech.ca>
2017-03-29 18:12:12 -0600

    Changed the project structure to follow the required one found on the docs

--- src/js/game.js
+++ src/js/game.js
@@ -3,7 +3,7 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload() {
-     game.load.image('snake_block', 'src/img/box.png');
+     game.load.image('snake_block', 'src/assets/box.png');
 
 }
 
